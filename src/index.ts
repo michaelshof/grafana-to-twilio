dbfeabbd915bca041c30d5c00b218be365dfb6ff
Michael Nowak <michael.nowak@sammatz.de>
2025-03-13 10:02:11 +0100

    Install twilio client

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import twilioSDK from 'twilio'
 
 dotenv.config()
 
-const httpd_port = process.env.HTTPD_PORT || 3000
+const httpd_config = {
+  port: process.env.HTTPD_PORT ? parseInt(process.env.HTTPD_PORT) : 3000,
+}
+
+const httpd_port = httpd_config.port
 const httpd = express()
 
 httpd.post('/call/contact/:id', (request, response) => {
@@ -18,8 +23,22 @@ const httpd_listener = httpd.listen(httpd_port, () => {
   console.log('HTTPD:', `Listening on port ${httpd_port}`)
 })
 
+const twilio_config = {
+  account_sid: process.env.TWILIO_ACCOUNT_SID || '',
+  auth_token: process.env.TWILIO_AUTH_TOKEN || '',
+  log_level: 'debug',
+}
+const twilio_client = twilioSDK(twilio_config.account_sid, twilio_config.auth_token, {
+  logLevel: twilio_config.log_level,
+})
+
+const twilio_balance = twilio_client.balance
+console.log('TWILIO:', 'Account Balance:', twilio_balance)
+
 process.on('SIGINT', () => {
   const promises: Array<Promise<boolean>> = []
+  console.log('')
+  console.log('SIGINT:', 'Processing signal')
 
   if (httpd_listener) {
     const httpd_promise = new Promise<boolean>((resolve) => {
