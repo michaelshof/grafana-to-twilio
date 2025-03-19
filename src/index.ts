45bd47dc9a138930b51777a4130bcb760164200e
Michael Nowak <michael.nowak@sammatz.de>
2025-03-19 14:03:49 +0100

    Prepone dotenv config; Prepone config logs

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,35 @@ import dotenv from 'dotenv'
 import ejs from 'ejs'
 import twilioSDK from 'twilio'
 
-import httpd from './httpd'
-import { Contact, Contacts, ContactGroups } from './types'
-import { ensure_file_paths_exist, ensure_contacts_valid, ensure_contact_groups_valid, ensure_twilio_credentials_valid } from './validations'
-
+dotenv.config()
 timestampPatch(console)
 
-dotenv.config()
+import httpd from './httpd'
+import { Contacts, ContactGroups } from './types'
+import { ensure_file_paths_exist, ensure_contacts_valid, ensure_contact_groups_valid, ensure_twilio_credentials_valid } from './validations'
+
 import { file_paths, httpd_config, twilio_config } from './configs'
 
 file_paths.contacts = fs.realpathSync(file_paths.contacts)
 file_paths.contact_groups = fs.realpathSync(file_paths.contact_groups)
 file_paths.twiml_ejs = fs.realpathSync(file_paths.twiml_ejs)
-ensure_file_paths_exist(file_paths)
 
 console.info('CONFIG:', 'File-Path Contacts:', file_paths.contacts)
 console.info('CONFIG:', 'File-Path Contact Groups:', file_paths.contact_groups)
 console.info('CONFIG:', 'File-Path TwiML EJS:', file_paths.twiml_ejs)
 
+console.info('CONFIG:', 'Express Port:', httpd_config.port)
+console.info('CONFIG:', 'Express Bearer-Token:', hide_inner_chars(httpd_config.bearer_token))
+console.info('CONFIG:', 'Express Rate-Window /call:', httpd_config.rate_window_call)
+console.info('CONFIG:', 'Express Rate-Limit /call:', httpd_config.rate_limit_call)
+
+console.info('CONFIG:', 'Twilio Account SID:', twilio_config.account_sid)
+console.info('CONFIG:', 'Twilio Auth Token:', hide_inner_chars(twilio_config.auth_token))
+console.info('CONFIG:', 'Twilio Phone Number:', twilio_config.phone_number)
+console.info('CONFIG:', 'Twilio Timeout:', twilio_config.timeout)
+console.info('CONFIG:', 'Twilio Log Level:', twilio_config.log_level)
+
+ensure_file_paths_exist(file_paths)
 const contacts_raw = JSON.parse(fs.readFileSync(file_paths.contacts, { encoding: 'utf-8' }))
 const contact_groups_raw = JSON.parse(fs.readFileSync(file_paths.contact_groups, { encoding: 'utf-8' }))
 
@@ -38,12 +49,6 @@ const twiml_ejs = fs.readFileSync(file_paths.twiml_ejs, { encoding: 'utf-8' })
 const twiml_template = ejs.compile(twiml_ejs)
 
 ensure_twilio_credentials_valid(twilio_config)
-console.info('CONFIG:', 'Twilio Account SID:', twilio_config.account_sid)
-console.info('CONFIG:', 'Twilio Auth Token:', hide_inner_chars(twilio_config.auth_token))
-console.info('CONFIG:', 'Twilio Phone Number:', twilio_config.phone_number)
-console.info('CONFIG:', 'Twilio Timeout:', twilio_config.timeout)
-console.info('CONFIG:', 'Twilio Log Level:', twilio_config.log_level)
-
 const twilio_client = twilioSDK(twilio_config.account_sid, twilio_config.auth_token, {
   logLevel: twilio_config.log_level,
 })
@@ -54,11 +59,6 @@ httpd.set('contacts', contacts)
 httpd.set('contact_groups', contact_groups)
 httpd.set('twilio_client', twilio_client)
 httpd.set('twiml_template', twiml_template)
-
-console.info('CONFIG:', 'Express Port:', httpd_config.port)
-console.info('CONFIG:', 'Express Bearer-Token:', hide_inner_chars(httpd_config.bearer_token))
-console.info('CONFIG:', 'Express Rate-Window /call:', httpd_config.rate_window_call)
-console.info('CONFIG:', 'Express Rate-Limit /call:', httpd_config.rate_limit_call)
 
 const httpd_port = httpd_config.port
 const httpd_listener = httpd.listen(httpd_port, () => {
